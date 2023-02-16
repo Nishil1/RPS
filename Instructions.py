bb75f2f9ed37f169e1cf30e246105632187e384f
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-02-17 09:15:48 +1300

    Create Instructions.py

---
+++ Instructions.py
@@ -0,0 +1,18 @@
+def show_instructions():
+    while True:
+        question = input("Have you played the game before? ")
+        instructions = ("Show Instructions")
+
+        if question == "yes" or question == "y":
+            print(instructions)
+            return
+        elif question == "no" or question == "n":
+            print(instructions)
+            return
+        else:
+            print("Please enter yes/no")
+
+
+
+
+
