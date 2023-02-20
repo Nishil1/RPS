4c37a611a46321bb0e971af6edd4dadfddb00016
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-02-21 11:36:39 +1300

    Update base.py

--- base.py
+++ base.py
@@ -1,83 +1,119 @@
 import random
 # functions go here
 
+# this function shows instructions when nesssacary
 def show_instructions():
     while True:
         question = input("Have you played the game before? ")
-        instructions = "Show Instructions"
-
         if question == "yes" or question == "y":
-
             return
         elif question == "no" or question == "n":
-            print(instructions)
+            print('show instruction')
             return
         else:
             print("Please enter yes/no")
 
+# Function make sures that the user input to the mode of gameplay is valid
+def capture_number_of_rounds(question):
+    while True:
+        type_of_play = input(question)
+        error = "Please click enter to enter infinity mode or set an amount of rounds"
+        if type_of_play != "":
+            try:
+                type_of_play = int(type_of_play)
+                if type_of_play < 1:
+                    print(error)
+                    continue
+            except ValueError:
+                print(error)
+                continue
+        return type_of_play
 
-
-def valid_input(question):
+# makes sures user input is valid eg.(rock, paper, scissors or "xxx" to quit)
+def capture_and_validate_user_input(question):
     while True:
         response = input(question).lower()
-        if response == "r" or response == "rock":
-            return response == "r"
-        elif response == "s" or response == "scissors":
-            return response == "s"
-        elif response == "p" or response == "paper":
-            return response == "p"
+        scissor = ["scissors", "scissor", "s"]
+        rock = ["r", "rock", "rocks"]
+        paper = ["p", "paper", "papers"]
+        if response in scissor:
+            return "scissors"
+        elif response in rock:
+            return "rock"
+        elif response in paper:
+            return "paper"
         elif response == "xxx":
-            quit()
-
-
+            quit("Exiting the game,Thanks for playing!")
         else:
-            print("Please enter a valid choice eg. rock, paper or scissors")
-
-def type_of_play(question):
-    while True:
-
-        response = input(question)
-        if response == "":
-            print("continuous mode")
-            return response
-
-        elif response == "a":
-           print()
-           return response
-        else:
-            print("Please enter a valid input to decide type of gameplay")
+            print("Please enter rock, paper or scissors")
 
 
-def who_wins(user, computer):
-    if user == computer:
-        print("draw")
-    elif user == "s" and computer == "p":
-        print("You won")
-    elif user == "p" and computer == "s":
-        print("you lost")
-    elif user == "r" and computer == "p":
-        print("You lost")
-    elif user == "p" and computer == "r":
-        print("You won")
-    elif user == "r" and computer == "s":
-        print("You won")
-    elif user == "s" and computer == "r":
-        print("You lost")
+def calculate_computer_input():
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    print('Computer choice: ', computer_choice)
+    return computer_choice;
+
+# takes the user input and prints/outputs the winner whilst generating a random value for the computer choice
+def decide_winner(user_choice,computer_choice):
+    if user_choice == computer_choice:
+        user_details['games_drawn'] += 1;
+
+        print('draw');
+    elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
+        user_details['games_won'] += 1;
+
+        print('user winner');
+    else:
+
+        user_details['games_lost'] += 1;
+        print('computer win')
+
+    user_details['number_of_rounds'] +=1;
 
 
-show_instructions()
-
-# set up addition game rules
-
-#start game
-
-play_type = type_of_play("Press <enter> to enter continuous "
-                "mode or press 'a' to set the amount of rounds")
+def display_results(user):
+    print("Games won: {}, Games lost: {}, Games drawn: {}".format(user['games_won'], user["games_lost"], user["games_drawn"]));
 
 
-user_choice = valid_input("Please choose rock, paper or scissors or type 'xxx' to quit: ")
+# This function contains gameplay components eg.(rounds, messages alerting
+# the user whether they won or lost etc) different to each mode
+def start_game():
+    rounds = 1
+    if amount_of_chosen_rounds != "": #user selected specific number of rounds
+        while rounds <= amount_of_chosen_rounds:
+            print("Round: {} of {}".format(rounds, amount_of_chosen_rounds))
+            user_choice = capture_and_validate_user_input("Choose rock, paper or scissors or enter 'xxx' to quit: ")
+            computer_choice = calculate_computer_input();
+            decide_winner(user_choice,computer_choice)
+            rounds += 1
+        display_results(user_details);
 
 
+    else:
+        while True:
+            print("Continuous Mode: Round {}".format(rounds))
+            user_choice = capture_and_validate_user_input("Choose rock, paper or scissors or enter 'xxx' to quit: ")
+            computer_choice = calculate_computer_input();
+            decide_winner(user_choice, computer_choice)
+            rounds += 1
+            display_results(user_details);
+
+# main routine starts here
+print("*********************************************")
+print("**** Welcome to Rock, Paper and Scissors ****")
+print("*********************************************")
+print()
+print()
+user_details = {
+      "name": "End User",
+      "games_won": 0,
+      "games_lost": 0,
+      "games_drawn": 0,
+      "number_of_rounds": 0
+}
 
 
-
+amount_of_chosen_rounds = capture_number_of_rounds\
+    ("Please press <enter> to enter infinite mode or enter # of rounds:  ")
+start_game();
