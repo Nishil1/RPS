2c8bc670c07188cdd1ab615983544d114282fecf
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-02-26 11:36:35 +1300

    Update base.py

--- base.py
+++ base.py
@@ -1,7 +1,9 @@
 import random
+
+
 # functions go here
 
-# this function shows instructions when nesssacary
+# this function shows instructions when nessacary
 def show_instructions():
     while True:
         question = input("Have you played the game before? ")
@@ -12,6 +14,7 @@ def show_instructions():
             return
         else:
             print("Please enter yes/no")
+
 
 # Function make sures that the user input to the mode of gameplay is valid
 def capture_number_of_rounds(question):
@@ -29,6 +32,7 @@ def capture_number_of_rounds(question):
                 continue
         return type_of_play
 
+
 # makes sures user input is valid eg.(rock, paper, scissors or "xxx" to quit)
 def capture_and_validate_user_input(question):
     while True:
@@ -37,67 +41,83 @@ def capture_and_validate_user_input(question):
         rock = ["r", "rock", "rocks"]
         paper = ["p", "paper", "papers"]
         if response in scissor:
-            return "scissors"
+            return "Scissors"
         elif response in rock:
-            return "rock"
+            return "Rock"
         elif response in paper:
-            return "paper"
+            return "Paper"
         elif response == "xxx":
+            print()
+            print("---------- Game History -----------")
+            display_results(user_details)
+
             quit("Exiting the game,Thanks for playing!")
         else:
             print("Please enter rock, paper or scissors")
 
 
 def calculate_computer_input():
-    options = ["rock", "paper", "scissors"]
+    options = ["Rock", "Paper", "Scissors"]
     computer_choice = random.choice(options)
-    print('Computer choice: ', computer_choice)
+    print('Computer choice:', computer_choice)
     return computer_choice;
 
+
 # takes the user input and prints/outputs the winner whilst generating a random value for the computer choice
-def decide_winner(user_choice,computer_choice):
+def decide_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         user_details['games_drawn'] += 1;
-
-        print('draw');
-    elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
+        print()
+        print('{} v {}, =========== draw ============='.format(user_choice, computer_choice));
+        print()
+    elif (user_choice == "Rock" and computer_choice == "Scissors") or (
+            user_choice == "Paper" and computer_choice == "Rock") or (
+            user_choice == "Scissors" and computer_choice == "Paper"):
         user_details['games_won'] += 1;
-
-        print('user winner');
+        print()
+        print("😀 😀 {} v {}. You won! 😀 😀  ".format(user_choice, computer_choice));
+        print()
     else:
-
         user_details['games_lost'] += 1;
-        print('computer win')
-
-    user_details['number_of_rounds'] +=1;
+        print()
+        print("XXXX {} v {}. You lost! XXXX".format(user_choice, computer_choice))
+        print()
+    user_details['number_of_rounds'] += 1;
 
 
 def display_results(user):
-    print("Games won: {}, Games lost: {}, Games drawn: {}".format(user['games_won'], user["games_lost"], user["games_drawn"]));
+    print("Games won: {}, Games lost: {}, Games drawn: {}".format(user['games_won'], user["games_lost"],
+                                                                  user["games_drawn"]));
 
 
 # This function contains gameplay components eg.(rounds, messages alerting
 # the user whether they won or lost etc) different to each mode
 def start_game():
     rounds = 1
-    if amount_of_chosen_rounds != "": #user selected specific number of rounds
+    if amount_of_chosen_rounds != "":  # user selected specific number of rounds
         while rounds <= amount_of_chosen_rounds:
             print("Round: {} of {}".format(rounds, amount_of_chosen_rounds))
-            user_choice = capture_and_validate_user_input("Choose rock, paper or scissors or enter 'xxx' to quit: ")
-            computer_choice = calculate_computer_input();
-            decide_winner(user_choice,computer_choice)
-            rounds += 1
-        display_results(user_details);
-
-
-    else:
-        while True:
-            print("Continuous Mode: Round {}".format(rounds))
+            print()
             user_choice = capture_and_validate_user_input("Choose rock, paper or scissors or enter 'xxx' to quit: ")
             computer_choice = calculate_computer_input();
             decide_winner(user_choice, computer_choice)
             rounds += 1
-            display_results(user_details);
+            print()
+        print("---------- Game History -----------")
+        display_results(user_details);
+    else:
+        while True:
+            print("Continuous Mode: Round {}".format(rounds))
+            print()
+            user_choice = capture_and_validate_user_input("Choose rock, paper or scissors or enter 'xxx' to quit: ")
+            computer_choice = calculate_computer_input();
+            decide_winner(user_choice, computer_choice)
+            rounds += 1
+        print()
+        print("---------- Game History -----------")
+        display_results(user_details);
+
+
 
 # main routine starts here
 print("*********************************************")
@@ -106,14 +126,15 @@ print("*********************************************")
 print()
 print()
 user_details = {
-      "name": "End User",
-      "games_won": 0,
-      "games_lost": 0,
-      "games_drawn": 0,
-      "number_of_rounds": 0
+
+    "games_won": 0,
+    "games_lost": 0,
+    "games_drawn": 0,
+    "number_of_rounds": 0
 }
 
-
-amount_of_chosen_rounds = capture_number_of_rounds\
+# gets number of rounds
+amount_of_chosen_rounds = capture_number_of_rounds \
     ("Please press <enter> to enter infinite mode or enter # of rounds:  ")
+# starts the game
 start_game();
